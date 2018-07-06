8a9f5d3f38cee0ac7098c6a68346d4a266d8b6ad
Jake <jake.horder@marquette.edu>
2018-07-06 10:19:48 -0400

    Hopefully added tour

---
+++ app-files/data.js
@@ -0,0 +1,186 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-31",
+      "name": "31",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": -1.2109122921872633,
+        "pitch": 0.18415727338155996,
+        "fov": 1.372389513909019
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.25955383677213817,
+          "pitch": 0.41444441585323943,
+          "rotation": 0,
+          "target": "1-29"
+        },
+        {
+          "yaw": 2.9618387786856966,
+          "pitch": 0.44684323716245444,
+          "rotation": 0,
+          "target": "2-32"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1367580404382256,
+          "pitch": -0.1975046463845409,
+          "title": "VICON Motion Capture System",
+          "text": "A 9-camera motion capture system for kinetic and kinematic analysis."
+        }
+      ]
+    },
+    {
+      "id": "1-29",
+      "name": "29",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 2.787051066042631,
+        "pitch": 0.21323126092363154,
+        "fov": 1.372389513909019
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2548178765346734,
+          "pitch": 0.5003963771424171,
+          "rotation": 0,
+          "target": "0-31"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0199961090254783,
+          "pitch": -0.24196266623168583,
+          "title": "Targeted Box &amp; Blocks Test",
+          "text": "<a href='http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0177965'>Normative data and comparison to standard tests for upper-limb analysis.</a>"
+        },
+        {
+          "yaw": -2.21816455240198,
+          "pitch": 0.39642807765132027,
+          "title": "3D Printer",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-32",
+      "name": "32",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": -0.6406142583347503,
+        "pitch": 0.1877800407950474,
+        "fov": 1.372389513909019
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.04823874547592055,
+          "pitch": 0.37368900154898377,
+          "rotation": 0,
+          "target": "0-31"
+        },
+        {
+          "yaw": -2.3159986726222233,
+          "pitch": 0.5222579651431669,
+          "rotation": 0,
+          "target": "3-34"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-34",
+      "name": "34",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": -2.9346335269078097,
+        "pitch": 0.2001725106751362,
+        "fov": 1.372389513909019
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8309477916387529,
+          "pitch": 0.4038607701958732,
+          "rotation": 0,
+          "target": "2-32"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.36885921684064193,
+          "pitch": 0.38452203990011036,
+          "title": "DEKA Arm",
+          "text": "<a href='https://newatlas.com/darpa-deka-luke-prosthetic-arm-mobius/44267/'> The DEKA Arm is the first prosthetic arm to translate sEMG signals into motion.</a>"
+        }
+      ]
+    }
+  ],
+  "name": "FPDU Lab",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
